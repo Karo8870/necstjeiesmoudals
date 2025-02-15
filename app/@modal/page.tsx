12765f4f80a898d66941e078d5987e395a123518
karo <cazacuchristian@gmail.com>
2025-02-15 20:03:45 +0200

    r

--- app/@modal/page.tsx
+++ app/@modal/page.tsx
@@ -1,13 +1,3 @@
-import Modal1 from './_modals/modal-1';
-import Modal2 from './_modals/modal-2';
-import Modal3 from './_modals/modal-3';
-
-const modals = {
-  'modal-1': Modal1,
-  'modal-2': Modal2,
-  'modal-3': Modal3
-};
-
 export default async function ({
   searchParams
 }: {
@@ -15,9 +5,9 @@ export default async function ({
 }) {
   const selectedModal = (await searchParams)['modal'];
 
-  if (selectedModal in modals) {
-    return <>{modals[selectedModal as keyof typeof modals]()}</>;
+  try {
+    return <>{await (await import(`./_modals/${selectedModal}`)).default()}</>;
+  } catch (e) {
+    return <></>;
   }
-
-  return <></>;
 }
